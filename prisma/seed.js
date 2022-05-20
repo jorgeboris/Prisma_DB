e1a180ecf2b049d82c1a7bffa9a3b6ae2b546188
jorgeboris <eduardo.borisq@gmail.com>
2022-05-19 23:24:03 -0500

    Agregando seed.js a prisma

---
+++ prisma/seed.js
@@ -0,0 +1,73 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+(async function main() {
+  try {
+    const woopa = await prisma.explorer.upsert({
+      where: { name: 'Woopa' },
+      update: {},
+      create: {
+        name: 'Woopa',
+				username: 'ajolonauta',
+				mission: 'Node'
+      },
+    });
+
+    const woopa1 = await prisma.explorer.upsert({
+      where: { name: 'Woopa1' },
+      update: {},
+      create: {
+        name: 'Woopa1',
+				username: 'ajolonauta1',
+				mission: 'Node'
+      },
+    });
+
+    const woopa2 = await prisma.explorer.upsert({
+      where: { name: 'Woopa 2' },
+      update: {},
+      create: {
+        name: 'Woopa 2',
+				username: 'ajolonauta2',
+				mission: 'Java'
+      },
+    });
+
+    const woopa3 = await prisma.explorer.upsert({
+      where: { name: 'Woopa 3' },
+      update: {},
+      create: {
+        name: 'Woopa 3',
+				username: 'ajolonauta3',
+				mission: 'Node'
+      },
+    });
+
+    const woopa4 = await prisma.explorer.upsert({
+        where: { name: 'Woopa4' },
+        update: {},
+        create: {
+            name: 'Woopa4',
+            username: 'ajolonauta4',
+            mission: 'Java'
+        }
+    });
+
+    const voryz = await prisma.explorer.upsert({
+        where: { name: 'Voryz' },
+        update: {},
+        create: {
+            name: 'Voryz',
+            username: 'boricuas',
+            mission: 'Node'
+        }
+    });
+
+    console.log('Create 6 explorers');
+  } catch(e) {
+    console.error(e);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+})();
