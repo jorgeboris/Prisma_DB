e78e60c80387d737ecdf114f03d78e78fe952cb9
jorgeboris <eduardo.borisq@gmail.com>
2022-05-20 00:43:41 -0500

    Agregando Modelo ExplorerData a la DB

---
+++ prisma/migrations/20220520054118_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "ExplorerData" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "lang" VARCHAR(255) NOT NULL,
+    "missionCommander" VARCHAR(255) NOT NULL,
+    "enrollments" INTEGER NOT NULL,
+    "hasCertification" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "ExplorerData_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ExplorerData_name_key" ON "ExplorerData"("name");
